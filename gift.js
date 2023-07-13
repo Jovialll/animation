0fe25eecb23070dd1f7b94d7953433d8a92f6935
Jovialll <lytjovial@gmail.com>
2023-07-13 10:40:36 +0800

    blabla

---
+++ gift.js
@@ -0,0 +1,18 @@
+const gifSection = document.querySelector('.gif-section');
+const subpageSection = document.querySelector('.subpage-section');
+let gifZoomed = false;
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    const gifSectionOffset = gifSection.offsetTop;
+    const windowHeight = window.innerHeight;
+
+    if (scrollPosition > gifSectionOffset && !gifZoomed) {
+        gifSection.querySelector('img').style.transform = 'scale(1.5)';
+        gifZoomed = true;
+    }
+
+    if (gifZoomed && scrollPosition > gifSectionOffset + windowHeight) {
+        subpageSection.classList.add('active');
+    }
+});
